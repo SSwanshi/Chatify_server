ae824a3e70f80f2c7094de36b4cf7786464f0323
Sarvjeet Swanshi <147067870+SSwanshi@users.noreply.github.com>
2026-03-27 02:29:53 +0530

    Update CORS origin to use HTTPS

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import Route from './routes/route.js'
 const app = express();
 
 app.use(cors({
-    origin: "http://chatify.swanshi.me",
+    origin: "https://chatify.swanshi.me",
     credentials: true
 }));
 
